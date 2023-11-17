8811b28acc3d65a5540cae14b17e0cb8d591f07e
Aman <akoua@umich.edu>
2023-11-17 15:49:38 -0500

    Successfully register chair

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,6 +1,119 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import backendURL from "../backendURL";
 
 const Admin = () => {
+  const [adminProfile, setAdminProfile] = useState({});
+
+  //
+
+  const [conferenceName, setConferenceName] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [country, setStaet] = useState("");
+  const [startDay, setStartDay] = useState("");
+  const [startMonth, setStartMonth] = useState("");
+  const [startYear, setStartYear] = useState("");
+  const [endDay, setEndDay] = useState("");
+  const [endMonth, setEndMonth] = useState("");
+  const [endYear, setEndYear] = useState("");
+  const [deadDay, setDeadDay] = useState("");
+  const [deadMonth, setDeadMonth] = useState("");
+  const [deadYear, setDeadYear] = useState("");
+
+  // chair state
+
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [title, setTitle] = useState("");
+  const [affiliation, setAffiliation] = useState("");
+
+  useEffect(() => {
+    // get admin user profile
+    if (!localStorage.getItem("user")) {
+      alert("cannot retrieve admin profile (not logged in)!");
+      return;
+    }
+
+    (async () => {
+      let user = JSON.parse(localStorage.getItem("user")!);
+
+      const response = await fetch(backendURL + "/user", {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+
+      const json = await response.json();
+      localStorage.setItem("profile", JSON.stringify(json));
+      console.log(json);
+    })();
+  }, []);
+
+  const handleRegisterConferenceAndChair = async () => {
+    if (!localStorage.getItem("user")) {
+      alert(
+        "cannot register new conference and chair because admin is not logged in!"
+      );
+    }
+
+    const user = JSON.parse(localStorage.getItem("user")!);
+    const token = user.token;
+
+    const startDate = `${startYear}-${startMonth}-${startDay}`;
+    const endDate = `${endYear}-${endMonth}-${endDay}`;
+    const deadline = `${deadYear}-${deadMonth}-${deadDay}`;
+
+    const chairResponse = await fetch(backendURL + "/admin/chair", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        email: email,
+        username: email.split("@")[0],
+        password: "password",
+        firstName: firstName,
+        lastName: lastName,
+        title: title,
+        affiliation: affiliation,
+        type: "Chair",
+      }),
+    });
+
+    let chairJSON = undefined;
+    let chairId = undefined;
+
+    try {
+      chairJSON = await chairResponse.json();
+      console.log(chairJSON);
+    } catch (e) {
+      alert("chair was not properly registerd!");
+      return;
+    }
+
+    return;
+
+    const response = await fetch(backendURL + "/admin/conference", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: conferenceName,
+        city: city,
+        state: state,
+        country: country,
+        startDate: startDate,
+        endDate: endDate,
+        submissionDeadline: deadline,
+      }),
+    });
+  };
+
   return (
     <div className="bg-gradient-to-b from-confPrimary to-gray-100 w-9/12 ml-auto mr-auto h-max">
       {/* Register new conference section */}
@@ -113,35 +226,70 @@ const Admin = () => {
         <div className="400 w-4/12">
           <h1 className="w-full text-3xl font-light">Email</h1>
         </div>
-        <input className="w-7/12 shadow-md" type="text" />
+        <input
+          className="w-7/12 shadow-md"
+          type="text"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
       </div>
       <div className="w-5/6 ml-auto mr-auto mt-5 flex flex-row justify-around">
         <div className="400 w-4/12">
           <h1 className="w-full text-3xl font-light">First Name</h1>
         </div>
-        <input className="w-7/12 shadow-md" type="text" />
+        <input
+          className="w-7/12 shadow-md"
+          type="text"
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+        />
       </div>
       <div className="w-5/6 ml-auto mr-auto mt-5 flex flex-row justify-around">
         <div className="400 w-4/12">
           <h1 className="w-full text-3xl font-light">Last Name</h1>
         </div>
-        <input className="w-7/12 shadow-md" type="text" />
+        <input
+          className="w-7/12 shadow-md"
+          type="text"
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />
       </div>
       <div className="w-5/6 ml-auto mr-auto mt-5 flex flex-row justify-around">
         <div className="400 w-4/12">
           <h1 className="w-full text-3xl font-light">Tile</h1>
         </div>
-        <input className="w-7/12 shadow-md" type="text" />
+        <input
+          className="w-7/12 shadow-md"
+          type="text"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
       </div>
       <div className="w-5/6 ml-auto mr-auto mt-5 flex flex-row justify-around">
         <div className="400 w-4/12">
           <h1 className="w-full text-3xl font-light">Affiliation</h1>
         </div>
-        <input className="w-7/12 shadow-md" type="text" />
+        <input
+          className="w-7/12 shadow-md"
+          type="text"
+          onChange={(e) => {
+            setAffiliation(e.target.value);
+          }}
+        />
       </div>
       <div className="w-2/4 h-max ml-auto mr-auto">
         <div className="w-max h-max ml-auto mr-auto">
-          <button className="w-full bg-confButton py-2 px-5 text-2xl hover:shadow-xl mt-5 mb-5">
+          <button
+            className="w-full bg-confButton py-2 px-5 text-2xl hover:shadow-xl mt-5 mb-5"
+            onClick={() => {
+              handleRegisterConferenceAndChair();
+            }}
+          >
             Register Conference & Chair
           </button>
         </div>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import backendURL from "../backendURL";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (email: string, password: string) => {
+    const response = await fetch(backendURL + "/auth", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+
+    try {
+      const json = await response.json();
+      localStorage.setItem("user", JSON.stringify(json));
+      console.log(json);
+
+      switch (
+        json.type // TODO : Navigate to each page depending on the user type!
+      ) {
+        case "Admin":
+          navigate("/admin");
+          break;
+        default:
+          return;
+      }
+    } catch (e) {
+      alert("login failed");
+    }
+  };
+
   return (
     <div className="bg-gradient-to-b from-confPrimary to-gray-100 w-9/12 ml-auto mr-auto h-screen">
       <div className="w-max ml-auto mr-auto pt-20">
@@ -10,16 +47,32 @@ const Login = () => {
         <h1 className="text-4xl font-light">Email</h1>
       </div>
       <div className="w-2/3 ml-auto mr-auto mt-3">
-        <input className="w-full h-10 p-2" />
+        <input
+          className="w-full h-10 p-2"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
       </div>
       <div className="w-2/3 ml-auto mr-auto mt-10">
         <h1 className="text-4xl font-light">Password</h1>
       </div>
       <div className="w-2/3 ml-auto mr-auto mt-3">
-        <input className="w-full h-10 p-2" />
+        <input
+          type="password"
+          className="w-full h-10 p-2"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
       </div>
       <div className="w-max ml-auto mr-auto mt-10">
-        <button className="bg-confPrimary shadow-md hover:shadow-xl pt-1 pb-1 pl-5 pr-5">
+        <button
+          className="bg-confPrimary shadow-md hover:shadow-xl pt-1 pb-1 pl-5 pr-5"
+          onClick={() => {
+            handleLogin(email, password);
+          }}
+        >
           Log In
         </button>
       </div>
